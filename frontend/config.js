a4878f5f38915c98bf042b0d3fd82dfaa02e62e4
Joshua Kennedy <josh@outthinkgroup.com>
2018-12-24 13:41:58 -0500

    added backend in frontend configd prod url

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
+export const frontEndpoint = `https://sickfits-yogo-prod.herokuapp.com/`;
 export const perPage = 4;
